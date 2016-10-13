b64d098461261614abe786deb2ad382578379c6d
Ken Ohata <ohataken@outlook.com>
2016-10-13 21:13:10 +0900

    define runi_object

--- runi_lisp.h
+++ runi_lisp.h
@@ -1,4 +1,50 @@
 #ifndef RUNI_LISP_H
 #define RUNI_LISP_H
 
+enum {
+    RUNI_INTEGER = 1,
+    RUNI_LIST,
+    RUNI_SYMBOL,
+    RUNI_PRIMITIVE,
+    RUNI_FUNCTION,
+    RUNI_MACRO,
+    RUNI_ENV,
+    RUNI_NIL,
+    RUNI_DOT,
+    RUNI_CPAREN,
+    RUNI_TRUE,
+};
+
+struct runi_object;
+
+typedef struct runi_object *runi_primitive(struct runi_object *env, struct runi_object *args);
+
+struct runi_object {
+    int type;
+
+    union {
+        int integer;
+
+        struct {
+            struct runi_object *car;
+            struct runi_object *cdr;
+        };
+
+        char name[1];
+
+        runi_primitive *fn;
+
+        struct {
+            struct runi_object *env;
+            struct runi_object *args;
+            struct runi_object *body;
+        };
+
+        struct {
+            struct runi_object *vars;
+            struct runi_object *parent;
+        };
+    };
+};
+
 #endif
